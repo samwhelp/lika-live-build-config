f1bcfc83d3216278a580148c7564c16d3370fb11
samwhelp <samwhelp@users.noreply.github.com>
2024-11-20 17:47:09 +0800

    fix

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 
-##sudo ./steps.sh --help
+##sudo ./steps.sh --help "${@}"
 
-##sudo ./steps.sh --verbose
+##sudo ./steps.sh --verbose "${@}"
 
-sudo ./steps.sh --verbose --debug
+sudo ./steps.sh --verbose --debug "${@}"
 
